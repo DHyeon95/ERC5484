97881ef557224c20a08f872459d062b318204148
DoHyeon <wjs505@pi-lab.io>
2023-11-20 13:21:22 +0900

    first commit

---
+++ contracts/test5484.sol
@@ -0,0 +1,20 @@
+
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.18;
+
+import "contracts/ERC5484.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+
+contract test5484 is ERC5484 , Ownable {
+
+    constructor(string memory name, string memory symbol) ERC721(name, symbol) Ownable(_msgSender()) {}
+
+    function testMint(address to , uint tokenId) public onlyOwner() {
+        _safeMint( to, tokenId );
+    }
+
+    function testBurn(uint tokenId) public {
+        _burnSbt( tokenId );
+    }
+
+}
